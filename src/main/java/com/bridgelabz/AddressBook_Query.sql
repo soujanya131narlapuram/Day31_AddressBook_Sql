19c214e44193dcdc9d1056ed43d21fbbf8ee6fbf
Soujanya <soujanya131narlapuram@gmail.com>
2023-01-22 14:04:21 +0530

    'Uc1 Commit'

--- src/main/java/com/bridgelabz/AddressBook_Query.sql
+++ src/main/java/com/bridgelabz/AddressBook_Query.sql
@@ -3,3 +3,16 @@ create database addressbook_service;
 show databases;
 use addressbook_service;
 
+--Uc2
+create table addressbook(
+id int not null auto_increment,
+first_name varchar(30) not null ,
+last_name varchar(30) not null ,
+address varchar(30) not null ,
+city varchar(30) not null ,
+state varchar(30) not null ,
+zip varchar(30) not null ,
+phone_number varchar(15) not null ,
+email varchar(30) not null ,
+primary key(id)
+);
